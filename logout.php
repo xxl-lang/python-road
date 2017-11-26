131d5e4ebec880fc9e3a8f4963e52a8534222e4f
xxl-lang <32210616+xxl-lang@users.noreply.github.com>
2017-11-27 07:52:32 +0800

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,14 @@
+<?php
+	session_start();
+	session_destroy();
+?>
+<script language="javascript">
+	alert("已经安全退出!");
+	location="login.php";
+</script>
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
